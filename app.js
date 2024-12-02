f6ce91c3108df29eb114b0d85824def068019851
Kayem <abdoulatuf.pro@gmail.com>
2024-12-02 14:32:19 +0100

    Add /users DELETE route to remove a user

--- app.js
+++ app.js
@@ -18,7 +18,16 @@ app.get('/date', (req, res) =>{
   res.send(`Current date and time: ${new Date()}`);
 });
 
-
+app.delete('/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const index = users.findIndex(user => user.id === userId);
+  if(index !== -1){
+    users.splice(index, 1);
+    res.status(200).send(`User ${userId} deleted`)
+  } else {
+    res.status(404).send('User not found');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
