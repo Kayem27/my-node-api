05a965e575d59ce4731f8e10446154f4b8233b5b
Kayem <abdoulatuf.pro@gmail.com>
2024-12-02 14:24:13 +0100

    Add /users POST route to add new user

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 
 app.get('/', (req, res) => {
   res.send('Hi everyone !');
 });
 
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+  newUser.id = req.length + 1;
+  res.push(newUser);
+  res.status(201).json(newUser);
+})
+
 app.get('/date', (req, res) =>{
   res.send(`Current date and time: ${new Date()}`);
 });
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
